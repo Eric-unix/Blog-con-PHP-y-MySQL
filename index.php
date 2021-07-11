2e2ea19b7597455316df7512ceb975c45b57f965
Kira <erixlopez6@gmail.com>
2021-07-11 17:41:15 -0500

    avances

--- index.php
+++ index.php
@@ -2,4 +2,13 @@
 require 'admin/config.php';
 require 'views/index.view.php';
 require 'funtions.php';
+$conexion = conexion($bd_config);
+if (!$conexion) {
+    header('Location: error.php');
+}
+$posts = obtener_post($blog_config['post_por_pagina'], $conexion);
+
+if (!$posts) {
+    header('Location: error.php');
+}
 ?> 
